03d57391b7faac6cda4c0b45686497507c79a734
MohitCode17 <mohit.codes17@gmail.com>
2025-01-31 21:45:05 +0530

    add add to cart button in product card

--- src/app/(home)/components/product-card.tsx
+++ src/app/(home)/components/product-card.tsx
@@ -15,6 +15,8 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import Image from "next/image";
 import React from "react";
 import ToppingList from "./topping-list";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
 
 export type Product = {
   id: string;
@@ -159,6 +161,14 @@ const ProductCard = ({ product }: PropTypes) => {
 
                 {/* Toppings */}
                 <ToppingList />
+
+                <div className="flex items-center justify-between mt-12">
+                  <span className="font-bold">₹400</span>
+                  <Button>
+                    <ShoppingCart size={20} />
+                    <span className="ml-2">Add to cart</span>
+                  </Button>
+                </div>
               </div>
             </div>
           </DialogContent>
