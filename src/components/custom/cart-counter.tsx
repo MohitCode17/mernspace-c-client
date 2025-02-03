7ed60309a09e7f07033356d2a18381c0d5e85011
MohitCode17 <mohit.codes17@gmail.com>
2025-02-03 17:39:33 +0530

    add cart item count logic

--- src/components/custom/cart-counter.tsx
+++ src/components/custom/cart-counter.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useAppSelector } from "@/lib/store/hooks";
 import { ShoppingBasket } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const CartCounter = () => {
+  const cartItemsCount = useAppSelector((state) => state.cart.cartItem);
+
   return (
     <>
       <div className="relative">
@@ -13,7 +16,7 @@ const CartCounter = () => {
           <ShoppingBasket size={20} className="hover:text-primary" />
         </Link>
         <span className="absolute -top-4 -right-2 h-5 w-5 text-xs flex items-center justify-center rounded-full bg-primary font-bold text-white">
-          3
+          {cartItemsCount.length}
         </span>
       </div>
     </>
