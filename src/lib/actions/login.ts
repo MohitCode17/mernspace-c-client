5768f206b2af1254e85003facf9492d797c1d508
MohitCode17 <mohit.codes17@gmail.com>
2025-02-06 20:38:07 +0530

    add login functionality

---
+++ src/lib/actions/login.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import { parse } from "cookie";
+import { cookies } from "next/headers";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function login(prevState: any, formdata: FormData) {
+  const email = formdata.get("email");
+  const password = formdata.get("password");
+
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/auth/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const error = await response.json();
+      console.log("Error while login", error);
+
+      return {
+        type: "error",
+        message: error.errors[0].message,
+      };
+    }
+
+    const c = response.headers.getSetCookie();
+    const accessToken = c.find((cookie) => cookie.includes("accessToken"));
+    const refreshToken = c.find((cookie) => cookie.includes("refreshToken"));
+
+    if (!accessToken || !refreshToken) {
+      return {
+        type: "error",
+        message: "No cookies were found!",
+      };
+    }
+
+    const parsedAccessToken = parse(accessToken);
+    const parsedRefreshToken = parse(refreshToken);
+
+    cookies().set("accessToken", parsedAccessToken.accessToken ?? "", {
+      expires: new Date(parsedAccessToken.Expires as string),
+      httpOnly: true,
+      path: parsedAccessToken.Path,
+      domain: parsedAccessToken.Domain,
+      sameSite: parsedAccessToken.SameSite as "strict",
+    });
+
+    cookies().set("refreshToken", parsedRefreshToken.refreshToken ?? "", {
+      expires: new Date(parsedRefreshToken.Expires as string),
+      httpOnly: true,
+      path: parsedRefreshToken.Path,
+      domain: parsedRefreshToken.Domain,
+      sameSite: parsedRefreshToken.SameSite as "strict",
+    });
+
+    return {
+      type: "success",
+      message: "Login successful!",
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    return {
+      type: "error",
+      message: err.message,
+    };
+  }
+}
