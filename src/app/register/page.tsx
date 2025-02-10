477788e831d8109b3c0aa00358e62927c4e0615b
MohitCode17 <mohit.codes17@gmail.com>
2025-02-10 19:32:07 +0530

    add register page ui

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,92 +1,63 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import login from "@/lib/actions/login";
-import { LoaderCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { useFormState, useFormStatus } from "react-dom";
 
-const SubmitButton = () => {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button>
-      {pending ? (
-        <div className="flex items-center gap-2">
-          <LoaderCircle className="animate-spin" />
-          <span>Please wait</span>
-        </div>
-      ) : (
-        "Login"
-      )}
-    </Button>
-  );
-};
-
-const initialState = {
-  type: "",
-  message: "",
-};
-
-const Login = () => {
-  const [state, formAction] = useFormState(login, initialState);
-
-  if (state.type === "success") {
-    window.location.href = "/";
-  }
-
+const Register = () => {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <p
+            {/* <p
               aria-live="polite"
               className={`${
                 state.type === "error" ? "text-red-500" : "text-green-500"
               }`}
             >
               {state.message}
-            </p>
-            <h1 className="text-3xl font-bold">Login</h1>
+            </p> */}
+            <h1 className="text-3xl font-bold">Register</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Enter your information to create an account
             </p>
           </div>
-          <form action={formAction}>
+          <form>
             <div className="grid gap-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="first-name">First name</Label>
+                  <Input id="first-name" placeholder="Max" required />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="last-name">Last name</Label>
+                  <Input id="last-name" placeholder="Robinson" required />
+                </div>
+              </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
                   type="email"
-                  name="email"
                   placeholder="m@example.com"
                   required
                 />
               </div>
               <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="/forgot-password"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
-                <Input id="password" name="password" type="password" required />
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
               </div>
-              <SubmitButton />
+              <Button type="submit" className="w-full">
+                Register
+              </Button>
             </div>
           </form>
           <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="/register" className="underline">
-              Sign up
+            Already have an account?{" "}
+            <Link href="/login" className="underline">
+              Sign in
             </Link>
           </div>
         </div>
@@ -105,4 +76,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
