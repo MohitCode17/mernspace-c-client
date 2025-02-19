3d8e87ae22cc5b4974c6d3e8443c4838530f143e
MohitCode17 <mohit.codes17@gmail.com>
2025-02-19 17:39:22 +0530

    clear cart if order success on card payment

---
+++ src/app/checkout/components/cart-cleaner.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { clearCart } from "@/lib/store/features/cart/cartSlice";
+import { useAppDispatch } from "@/lib/store/hooks";
+import { useEffect } from "react";
+
+const CartCleaner = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(clearCart());
+  }, [dispatch]);
+
+  return null;
+};
+
+export default CartCleaner;
--- src/lib/store/features/cart/cartSlice.ts
+++ src/lib/store/features/cart/cartSlice.ts
@@ -77,10 +77,18 @@ export const cartSlice = createSlice({
 
       window.localStorage.setItem("cartItems", JSON.stringify(state.cartItem));
     },
+
+    clearCart: () => {
+      window.localStorage.setItem("cartItems", JSON.stringify([]));
+      return {
+        cartItem: [],
+      };
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToCart, setInitialCartItems, changeQty } = cartSlice.actions;
+export const { addToCart, setInitialCartItems, changeQty, clearCart } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
