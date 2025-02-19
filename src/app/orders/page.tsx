eec524f650946b810228182471736066a460a599
MohitCode17 <mohit.codes17@gmail.com>
2025-02-19 19:39:51 +0530

    show no orders message

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -33,7 +33,7 @@ const Orders = async () => {
     throw new Error("Error fetching my orders.");
   }
 
-  const orders = await response.json();
+  const orders = (await response.json()) || [];
 
   return (
     <div className="container mt-8">
@@ -43,48 +43,52 @@ const Orders = async () => {
           <CardDescription>My complete order history.</CardDescription>
         </CardHeader>
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">ID</TableHead>
-                <TableHead>Payment Status</TableHead>
-                <TableHead>Payment Method</TableHead>
-                <TableHead>Date Time</TableHead>
-                <TableHead>Order Status</TableHead>
-                <TableHead>Amount</TableHead>
-                <TableHead className="text-right">Details</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {orders.map((order: Order) => {
-                return (
-                  <TableRow key={order._id}>
-                    <TableCell className="font-medium">{order._id}</TableCell>
-                    <TableCell>{order.paymentStatus.toUpperCase()}</TableCell>
-                    <TableCell>{order.paymentMode.toLowerCase()}</TableCell>
-                    <TableCell>
-                      {new Date(order.createdAt).toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      <Badge variant={"outline"}>
-                        {order.orderStatus.toUpperCase()}
-                      </Badge>
-                    </TableCell>
-                    {/* TODO: MAKE SURE TOTAL IS GRAND TOTAL AFTER DISCOUNT, TAXES AND DELIVERY CHARGES */}
-                    <TableCell>₹{order.total}</TableCell>
-                    <TableCell className="text-right">
-                      <Link
-                        href={`/order/${order._id}`}
-                        className="underline text-primary"
-                      >
-                        More details
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+          {orders.length === 0 ? (
+            <p>No orders yet</p>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">ID</TableHead>
+                  <TableHead>Payment Status</TableHead>
+                  <TableHead>Payment Method</TableHead>
+                  <TableHead>Date Time</TableHead>
+                  <TableHead>Order Status</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead className="text-right">Details</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {orders.map((order: Order) => {
+                  return (
+                    <TableRow key={order._id}>
+                      <TableCell className="font-medium">{order._id}</TableCell>
+                      <TableCell>{order.paymentStatus.toUpperCase()}</TableCell>
+                      <TableCell>{order.paymentMode.toLowerCase()}</TableCell>
+                      <TableCell>
+                        {new Date(order.createdAt).toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        <Badge variant={"outline"}>
+                          {order.orderStatus.toUpperCase()}
+                        </Badge>
+                      </TableCell>
+                      {/* TODO: MAKE SURE TOTAL IS GRAND TOTAL AFTER DISCOUNT, TAXES AND DELIVERY CHARGES */}
+                      <TableCell>₹{order.total}</TableCell>
+                      <TableCell className="text-right">
+                        <Link
+                          href={`/order/${order._id}`}
+                          className="underline text-primary"
+                        >
+                          More details
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
         </CardContent>
       </Card>
     </div>
