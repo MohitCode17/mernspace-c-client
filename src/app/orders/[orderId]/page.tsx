2d1e3083f67e496007d77a6ca25d49a9fa7aaa61
MohitCode17 <mohit.codes17@gmail.com>
2025-02-20 23:20:19 +0530

    fetch single order and render

--- src/app/orders/[orderId]/page.tsx
+++ src/app/orders/[orderId]/page.tsx
@@ -10,8 +10,25 @@ import { Separator } from "@/components/ui/separator";
 import { Banknote, Coins, LayoutDashboard } from "lucide-react";
 import React from "react";
 import OrderStatus from "./components/order-status";
+import { cookies } from "next/headers";
+import { Order } from "@/lib/types";
 
-const SingleOrder = () => {
+const SingleOrder = async ({ params }: { params: { orderId: string } }) => {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/order/orders/${params.orderId}?fields=address,paymentStatus,paymentMode`,
+    {
+      headers: {
+        Authorization: `Bearer ${cookies().get("accessToken")?.value}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch single order");
+  }
+
+  const order: Order = await response.json();
+
   return (
     <div className="container mt-6 flex flex-col gap-6">
       <Card>
@@ -33,10 +50,10 @@ const SingleOrder = () => {
           </CardHeader>
           <Separator />
           <CardContent className="pt-6">
-            <h2 className="font-bold">Rohit Sharma</h2>
-            <p className="mt-2">
-              55, New Street, upper lane, New Delhi. India. 409876
-            </p>
+            <h2 className="font-bold">
+              {order.customerId.firstName + " " + order.customerId.lastName}
+            </h2>
+            <p className="mt-2">{order.address}</p>
           </CardContent>
         </Card>
 
@@ -51,19 +68,19 @@ const SingleOrder = () => {
             <div className="flex items-center gap-2">
               <LayoutDashboard size={20} />
               <h2 className="text-base font-medium">Order reference: </h2>
-              ord121313123131313
+              {order._id}
             </div>
 
             <div className="flex items-center gap-2 mt-2">
               <Banknote />
               <h2 className="text-base font-medium">Payment status: </h2>
-              <span>Paid</span>
+              <span>{order.paymentStatus.toUpperCase()}</span>
             </div>
 
             <div className="flex items-center gap-2 mt-2">
               <Coins size={20} />
               <h2 className="text-base font-medium">Payment method: </h2>
-              <span>Card</span>
+              <span>{order.paymentMode.toUpperCase()}</span>
             </div>
 
             <Button variant={"destructive"} className="mt-6">
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -91,7 +91,7 @@ export type OrderData = {
 
 export interface Order {
   _id: string;
-  customerId: string;
+  customerId: Customer;
   total: number;
   discount: number;
   taxes: number;
